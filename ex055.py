d055dc8ca86bc578222cc7dfc9bc47541ce6819d
Geovana Nava <101190766+geovnv@users.noreply.github.com>
2022-08-30 19:46:42 -0300

    Add files via upload

---
+++ ex055.py
@@ -0,0 +1,14 @@
+pmaior = 0
+pmenor = 0
+for p in range (1, 6):
+    peso = float(input('Qual o peso da {}ª pessoa? '.format(p)))
+    if p == 1:
+        pmaior = peso
+        pmenor = peso
+    else:
+        if peso > pmaior:
+            pmaior = peso
+        if peso < pmenor:
+            pmenor = peso
+print('O maior peso digitado foi {}Kg'.format(pmaior))
+print('O menor peso digitado foi {}Kg'.format(pmenor))
