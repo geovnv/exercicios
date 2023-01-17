c6d97402c669f6dc3b90aadcca7c29628c0a3c20
Geovana Nava <101190766+geovnv@users.noreply.github.com>
2023-01-17 20:32:09 -0300

    Add files via upload

---
+++ ex073.py
@@ -0,0 +1,13 @@
+colocados = ('Palmeiras', 'Flamengo', 'Corinthians', 'Internacional',
+    'Fluminense', 'Athletico-PR', 'Atlético-MG', 'América-MG',
+    'Goiás', 'Santos', 'Bragantino', 'Fortaleza', 'Botafogo', 'São Paulo',
+    'Ceará', 'Cuiabá', 'Coritiba', 'Avaí', 'Atletico-GO', 'Juventude)',)
+print('=-' * 20)
+print(f'Lista de times do Brasileirão: {colocados}')
+print('=-' * 20)
+print(f'Os cinco primeiros são: {colocados[0:5]}')
+print('=-' * 20)
+print(f'Os quatro últimos são: {colocados[16:]}')
+print('=-' * 20)
+print(f'Times em ordem alfabética: ', sorted(colocados))
+print('=-' * 20)
